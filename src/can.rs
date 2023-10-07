305b2773dd8009798eea5c66cdb3bbd3e0c5e7da
Kaizen Sparc <kaizenwoof@integralement.nu>
2023-10-07 20:37:19 +0200

    Fix CAN implementation

--- src/can.rs
+++ src/can.rs
@@ -5,34 +5,28 @@
 //! TX: Alternate Push-Pull Output
 //! RX: Input Floating Input
 //!
+//! ### CAN0
+//!
+//! | Function | NoRemap |     Remap     |
+//! |----------|---------|---------------|
+//! | TX       | PA12    | PB9   | PD1   |
+//! | RX       | PA11    | PB8   | PD0   |
+//!
 //! ### CAN1
 //!
 //! | Function | NoRemap | Remap |
 //! |----------|---------|-------|
-//! | TX       | PA12    | PB9   |
-//! | RX       | PA11    | PB8   |
-//!
-//! ### CAN2
-//!
-//! | Function | NoRemap | Remap |
-//! |----------|---------|-------|
-//! | TX       | PB6     | PB13  |
-//! | RX       | PB5     | PB12  |
+//! | TX       | PB13    | PB6   |
+//! | RX       | PB12    | PB5   |
 
-use crate::afio::MAPR;
-#[cfg(feature = "connectivity")]
-use crate::gpio::gpiob::{PB12, PB13, PB5, PB6};
 use crate::gpio::{
     gpioa::{PA11, PA12},
-    gpiob::{PB8, PB9},
+    gpiob::{PB12, PB13, PB5, PB6, PB8, PB9},
+    gpiod::{PD0, PD1},
     Alternate, Floating, Input, PushPull,
 };
-use crate::pac::CAN1;
-#[cfg(feature = "connectivity")]
-use crate::pac::CAN2;
-#[cfg(not(feature = "connectivity"))]
-use crate::pac::USB;
-use crate::rcc::APB1;
+use crate::pac::{AFIO, CAN0, CAN1};
+use crate::rcu::APB1;
 
 mod sealed {
     pub trait Sealed {}
@@ -40,52 +34,53 @@ mod sealed {
 
 pub trait Pins: sealed::Sealed {
     type Instance;
-    fn remap(mapr: &mut MAPR);
+    fn remap(afio: &mut AFIO);
 }
 
 impl sealed::Sealed for (PA12<Alternate<PushPull>>, PA11<Input<Floating>>) {}
 impl Pins for (PA12<Alternate<PushPull>>, PA11<Input<Floating>>) {
-    type Instance = CAN1;
+    type Instance = CAN0;
 
-    fn remap(mapr: &mut MAPR) {
-        #[cfg(not(feature = "connectivity"))]
-        mapr.modify_mapr(|_, w| unsafe { w.can_remap().bits(0) });
-        #[cfg(feature = "connectivity")]
-        mapr.modify_mapr(|_, w| unsafe { w.can1_remap().bits(0) });
+    fn remap(afio: &mut AFIO) {
+        afio.pcf0.modify(|_, w| unsafe { w.can0_remap().bits(0) });
     }
 }
 
 impl sealed::Sealed for (PB9<Alternate<PushPull>>, PB8<Input<Floating>>) {}
 impl Pins for (PB9<Alternate<PushPull>>, PB8<Input<Floating>>) {
+    type Instance = CAN0;
+
+    fn remap(afio: &mut AFIO) {
+        afio.pcf0
+            .modify(|_, w| unsafe { w.can0_remap().bits(0b10) });
+    }
+}
+
+impl sealed::Sealed for (PD1<Alternate<PushPull>>, PD0<Input<Floating>>) {}
+impl Pins for (PD1<Alternate<PushPull>>, PD0<Input<Floating>>) {
+    type Instance = CAN0;
+
+    fn remap(afio: &mut AFIO) {
+        afio.pcf0
+            .modify(|_, w| unsafe { w.can0_remap().bits(0b11) });
+    }
+}
+
+impl sealed::Sealed for (PB13<Alternate<PushPull>>, PB12<Input<Floating>>) {}
+impl Pins for (PB13<Alternate<PushPull>>, PB12<Input<Floating>>) {
     type Instance = CAN1;
 
-    fn remap(mapr: &mut MAPR) {
-        #[cfg(not(feature = "connectivity"))]
-        mapr.modify_mapr(|_, w| unsafe { w.can_remap().bits(0b10) });
-        #[cfg(feature = "connectivity")]
-        mapr.modify_mapr(|_, w| unsafe { w.can1_remap().bits(0b10) });
+    fn remap(afio: &mut AFIO) {
+        afio.pcf0.modify(|_, w| w.can1_remap().clear_bit());
     }
 }
 
-#[cfg(feature = "connectivity")]
-impl sealed::Sealed for (PB13<Alternate<PushPull>>, PB12<Input<Floating>>) {}
-#[cfg(feature = "connectivity")]
-impl Pins for (PB13<Alternate<PushPull>>, PB12<Input<Floating>>) {
-    type Instance = CAN2;
-
-    fn remap(mapr: &mut MAPR) {
-        mapr.modify_mapr(|_, w| w.can2_remap().clear_bit());
-    }
-}
-
-#[cfg(feature = "connectivity")]
 impl sealed::Sealed for (PB6<Alternate<PushPull>>, PB5<Input<Floating>>) {}
-#[cfg(feature = "connectivity")]
 impl Pins for (PB6<Alternate<PushPull>>, PB5<Input<Floating>>) {
-    type Instance = CAN2;
+    type Instance = CAN1;
 
-    fn remap(mapr: &mut MAPR) {
-        mapr.modify_mapr(|_, w| w.can2_remap().set_bit());
+    fn remap(afio: &mut AFIO) {
+        afio.pcf0.modify(|_, w| w.can1_remap().set_bit());
     }
 }
 
@@ -96,46 +91,33 @@ pub struct Can<Instance> {
 
 impl<Instance> Can<Instance>
 where
-    Instance: crate::rcc::Enable<Bus = APB1>,
+    Instance: crate::rcu::Enable<Bus = APB1>,
 {
     /// Creates a CAN interaface.
-    ///
-    /// CAN shares SRAM with the USB peripheral. Take ownership of USB to
-    /// prevent accidental shared usage.
-    #[cfg(not(feature = "connectivity"))]
-    pub fn new(can: Instance, apb: &mut APB1, _usb: USB) -> Can<Instance> {
-        Instance::enable(apb);
-        Can { _peripheral: can }
-    }
-
-    /// Creates a CAN interaface.
-    #[cfg(feature = "connectivity")]
     pub fn new(can: Instance, apb: &mut APB1) -> Can<Instance> {
         Instance::enable(apb);
         Can { _peripheral: can }
     }
 
     /// Routes CAN TX signals and RX signals to pins.
-    pub fn assign_pins<P>(&self, _pins: P, mapr: &mut MAPR)
+    pub fn assign_pins<P>(&self, _pins: P, afio: &mut AFIO)
     where
         P: Pins<Instance = Instance>,
     {
-        P::remap(mapr);
+        P::remap(afio);
     }
+}
+
+unsafe impl bxcan::Instance for Can<CAN0> {
+    const REGISTERS: *mut bxcan::RegisterBlock = CAN0::ptr() as *mut _;
 }
 
 unsafe impl bxcan::Instance for Can<CAN1> {
     const REGISTERS: *mut bxcan::RegisterBlock = CAN1::ptr() as *mut _;
 }
 
-#[cfg(feature = "connectivity")]
-unsafe impl bxcan::Instance for Can<CAN2> {
-    const REGISTERS: *mut bxcan::RegisterBlock = CAN2::ptr() as *mut _;
-}
-
-unsafe impl bxcan::FilterOwner for Can<CAN1> {
+unsafe impl bxcan::FilterOwner for Can<CAN0> {
     const NUM_FILTER_BANKS: u8 = 28;
 }
 
-#[cfg(feature = "connectivity")]
-unsafe impl bxcan::MasterInstance for Can<CAN1> {}
+unsafe impl bxcan::MasterInstance for Can<CAN0> {}
--- src/lib.rs
+++ src/lib.rs
@@ -58,7 +58,7 @@ pub use gd32e1::gd32e103 as pac;
 
 pub mod adc;
 //pub mod backup_domain;
-//pub mod can;
+pub mod can;
 pub mod crc;
 pub mod delay;
 pub mod dma;
